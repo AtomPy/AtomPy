03cbedfc5ca3a70b28941e94292a4accd7f1dfbd
JosiahBoswell <josiah.s.boswell@gmail.com>
2013-10-17 12:37:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ setup(
     license='LICENSE.txt',
     description='A cloud atomic data service for astrophysical applications.',
     long_description = open('atompy/README.txt').read(),
+    packages=['atompy'],
     install_requires = [
         "xlrd >= 0.9.2",
         "google-api-python-client >= 0.0.0",
         "pandas >= 0.11.0",
-		"xlwt >= 0.0.0"
+	"xlwt >= 0.0.0"
     ],
 )
