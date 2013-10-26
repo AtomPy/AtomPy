4fa0345fca03aa70a74e7fbaa45cb49d2a689a5a
Josiah Boswell <josiah.s.boswell@gmail.com>
2013-10-25 22:41:16 -0400

    updated installation file

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     name = 'AtomPy',
     version = '0.5.0',
     author = 'Josiah Boswell',
-    license='LICENSE.txt',
     description='A cloud atomic data service for astrophysical applications.',
-    long_description = open('atompy/README.txt').read(),
     packages=['atompy'],
     install_requires = [
         "xlrd >= 0.9.2",
