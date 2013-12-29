2bb61694013380670eb5cb4686c2a0d66a8377a7
JosiahBoswell <josiah.s.boswell@gmail.com>
2013-12-29 14:22:05 -0500

    Version change update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'AtomPy',
-    version = '0.5.0',
+    version = '0.5.1',
     author = 'Josiah Boswell',
     description='A cloud atomic data service for astrophysical applications.',
     packages=['atompy'],
